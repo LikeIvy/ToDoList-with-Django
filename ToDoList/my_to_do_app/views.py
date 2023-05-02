a43d06cc2e76024e3156d38b155b139d0d098e28
LikeIvy <ivy74115@gmail.com>
2023-05-02 17:20:46 +0900

    complete_ToDoList

---
+++ ToDoList/my_to_do_app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect # HttpResponse => 단순히 인자로 받은 문자열을 사용자의 화면에 보여 주도록 하는 함수
+from django.urls import reverse
+from .models import *                # models앞의 .은 같은 위치라는 의미
+
+# Create your views here.
+def index(request):
+    todos = Todo.objects.all()
+    content = {"todos" : todos}
+    return render(request, "my_to_do_app/index.html", content)
+
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+    new_todo = Todo(content = user_input_str)
+    new_todo.save()
+    return HttpResponseRedirect(reverse("index"))
+    # return HttpResponse("create Todo를 할 거야! =>"+user_input_str)
+
+def deleteTodo(request):
+    done_todo_id = request.GET['todoNum']
+    print('완료한 todo의 id', done_todo_id)
+    todo = Todo.objects.get(id = done_todo_id)
+    todo.delete()
+    return HttpResponseRedirect(reverse('index'))
+    
